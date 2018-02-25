71dd135687b64888005da3122d64db2b9f8f7201
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-25 15:31:31 +0200

    Initial commit

---
+++ src/js/jquery.main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$('#owl-testimonial').owlCarousel({
+		loop: true,
+		items: 1,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+	});
+
+	if($(window).width() < 1025){
+
+		$('#portfolio-grid').owlCarousel({
+			loop: true,
+			items: 1,
+			autoplay: false,
+			autoplayTimeout: 3000,
+			dots: false,
+			nav: true,
+			navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+			responsive: {
+				768: {
+					items: 2,
+					margin: 15
+				}
+			}
+		});
+	}
+});	
