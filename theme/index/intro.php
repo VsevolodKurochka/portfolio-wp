38968b5c6f67f9370a5d75a79239b5a47cf73aa6
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-25 15:55:43 +0200

    Add intro & skills sections

---
+++ theme/index/intro.php
@@ -0,0 +1,43 @@
+<section class="intro" id="section-intro">
+	<div class="intro__shape intro__shape_left shape shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_white">
+			</div>
+		</div>
+	</div>
+	<div class="intro__shape intro__shape_right shape shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_white">
+			</div>
+		</div>
+	</div>
+	<div class="intro__container container">
+		<div class="intro__shape intro__shape_main shape" data-emergence="hidden">
+			<div class="shape__inner">
+				<div class="shape__inner-shape shape__inner-shape_brand-1">
+				</div>
+			</div>
+		</div>
+		<div class="intro__row row">
+			<div class="intro__column col-12 col-sm-6 col-lg-5">
+				<h1 class="intro__title" data-emergence="hidden" data-text="Vsevolod Kurochka">Vsevolod Kurochka
+				</h1>
+				<!--+e.H1.title
+				| Vsevolod
+				br
+				span.font-weight-extra-light Kurochka
+				| Kurochka
+				-->
+				<h2 class="intro__subtitle"><span class="intro__subtitle-element">Web developer</span><span class="intro__subtitle-element">Freelancer</span><span class="intro__subtitle-element">4 years experience</span>
+				</h2>
+				<p class="intro__content">Looking for a web developer? I am a freelancer from Cherkassy, Ukraine. With the help of <span class="highlight highlight_brand-1">magic </span>, I will create a modern site for your business.
+				</p>
+				<div class="intro__button">
+					<button class="btn btn_cross btn_cross-white btn_size-lg"><span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+						<p>My resume</p>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ theme/index/skills.php
@@ -0,0 +1,54 @@
+<section class="skills section-anchor" id="section-skills">
+	<div class="skills__shape skills__shape_right shape shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_brand-1">
+			</div>
+		</div>
+	</div>
+	<div class="skills__container container">
+		<div class="skills__row row">
+			<div class="col-12 col-sm-7">
+				<div class="group">
+					<h3 class="group__title" data-emergence="hidden" data-text="Here you can see">Here you can see <span class="group__title-element">my skills</span>
+					</h3>
+				</div>
+			</div>
+			<div class="col-12 col-sm-4">
+				<ul class="skills__items">
+					<li class="skills__item" data-emergence="hidden">
+						<p class="skills__item-header">Wordpress
+						</p>
+						<div class="skills__item-line"><span class="skills__item-line-slider" style="width:85%;"></span>
+						</div>
+					</li>
+					<li class="skills__item" data-emergence="hidden">
+						<p class="skills__item-header">CSS/CSS3
+						</p>
+						<div class="skills__item-line"><span class="skills__item-line-slider" style="width:90%;"></span>
+						</div>
+					</li>
+					<li class="skills__item" data-emergence="hidden">
+						<p class="skills__item-header">HTML/HTML5
+						</p>
+						<div class="skills__item-line"><span class="skills__item-line-slider" style="width:95%;"></span>
+						</div>
+					</li>
+					<li class="skills__item" data-emergence="hidden">
+						<p class="skills__item-header">JS/JQUERY
+						</p>
+						<div class="skills__item-line"><span class="skills__item-line-slider" style="width:84%;"></span>
+						</div>
+					</li>
+					<li class="skills__item" data-emergence="hidden">
+						<p class="skills__item-header">PHP
+						</p>
+						<div class="skills__item-line"><span class="skills__item-line-slider" style="width:60%;"></span>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<p class="skills__letters-bg letters-bg" data-emergence="hidden">Pro
+		</p>
+	</div>
+</section>
