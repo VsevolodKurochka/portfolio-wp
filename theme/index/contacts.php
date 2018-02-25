f346593f22d6434831d0056ab366ccc21b15f15d
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-25 15:59:01 +0200

    Add contacts part

---
+++ theme/index/contacts.php
@@ -0,0 +1,57 @@
+<section class="contacts" id="section-contacts">
+	<div class="contacts__shape contacts__shape_left shape shape_brand-1 shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_brand-1">
+			</div>
+		</div>
+	</div>
+	<div class="contacts__container container">
+		<div class="contacts__row row">
+			<div class="col-12 col-sm-6">
+				<div class="contacts__group group">
+					<h3 class="group__title" data-emergence="hidden" data-text="Let’s work Together">Let’s work <span class="color-brand-1">Together</span>
+					</h3>
+				</div>
+				<ul class="social social_big">
+					<li class="social__item"><a class="social__item-link" href="tel: +38 (063) 505 62 63"><i class="social__item-link-icon fas fa-mobile" aria-hidden="true"></i></a>
+					</li>
+					<li class="social__item"><a class="social__item-link" href="mailto: seva.kurochka@gmail.com"><i class="social__item-link-icon fas fa-envelope" aria-hidden="true"></i></a>
+					</li>
+					<li class="social__item"><a class="social__item-link" href="skype: mozilla20103"><i class="social__item-link-icon fab fa-skype" aria-hidden="true"></i></a>
+					</li>
+					<li class="social__item"><a class="social__item-link" href="https://www.facebook.com/vsevolod.kurochka.1"><i class="social__item-link-icon fab fa-facebook" aria-hidden="true"></i></a>
+					</li>
+					<li class="social__item"><a class="social__item-link" href="https://github.com/VsevolodKurochka"><i class="social__item-link-icon fab fa-github" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-12 col-sm-6 col-md-5">
+				<div class="message message_shadow">
+					<div class="message__header">
+						<p class="message__title">Start working with me
+						</p>
+					</div>
+					<div class="message__body">
+						<form class="form form_block">
+							<div class="form__content">
+								<div class="form__row form__row_line"><input class="form__control" name="vname" type="text" placeholder="Name"/><span class="form__line"></span>
+								</div>
+								<div class="form__row form__row_line"><input class="form__control" name="vemail" type="email" placeholder="E-mail"/><span class="form__line"></span>
+								</div>
+								<div class="form__row form__row_line"><input class="form__control" name="vphone" type="phone" placeholder="Phone"/><span class="form__line"></span>
+								</div>
+								<div class="form__row">
+									<button class="btn btn_cross btn_cross-brand-1 btn_size-lg" type="submit"><span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+										<p>Send</p>
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<p class="contacts__letters-bg letters-bg" data-emergence="hidden">Contact
+		</p>
+	</div>
+</section>
