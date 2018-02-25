bb4d24dc57c49252c272b4daa1f76cc0c60b0bc4
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-25 16:34:08 +0200

    Services items with foreach

--- theme/index/services.php
+++ theme/index/services.php
@@ -1,3 +1,31 @@
+<?php
+	$items = [
+		[
+			'icon'	=> 'theme/components/icons/service-1',
+			'title'	=> 'Creating a turnkey website'
+		],
+		[
+			'icon'	=> 'theme/components/icons/service-2',
+			'title'	=> 'Creation of selling landings'
+		],
+		[
+			'icon'	=> 'theme/components/icons/service-3',
+			'title'	=> 'Quality and adaptive design'
+		],
+		[
+			'icon'	=> 'theme/components/icons/service-4',
+			'title'	=> 'Programming (WordPress, Opencart)'
+		],
+		[
+			'icon'	=> 'theme/components/icons/service-5',
+			'title'	=> 'Copywriting'
+		],
+		[
+			'icon'	=> 'theme/components/icons/service-6',
+			'title'	=> 'Support for existing projects'
+		]
+	];
+?>
 <section class="services" id="section-services">
 	<div class="container">
 		<div class="group group_inline">
@@ -5,47 +33,14 @@
 			</h3>
 		</div>
 		<div class="services__grid">
-			<div class="services__item" data-emergence="hidden">
-				<div class="services__item-header">
-					<?php get_template_part('theme/components/icons/service-1'); ?>
+			<?php foreach ($items as $item) : ?>
+				<div class="services__item" data-emergence="hidden">
+					<div class="services__item-header">
+						<?php get_template_part($item['icon']); ?>
+					</div>
+					<p class="services__item-title"><?php echo $item['title']; ?></p>
 				</div>
-				<p class="services__item-title">Creating a turnkey website</p>
-			</div>
-			<div class="services__item" data-emergence="hidden">
-				<div class="services__item-header">
-					<?php get_template_part('theme/components/icons/service-2'); ?>
-				</div>
-				<p class="services__item-title">Creation of selling landings
-				</p>
-			</div>
-			<div class="services__item" data-emergence="hidden">
-				<div class="services__item-header">
-					<?php get_template_part('theme/components/icons/service-3'); ?>
-				</div>
-				<p class="services__item-title">Quality and adaptive design
-				</p>
-			</div>
-			<div class="services__item" data-emergence="hidden">
-				<div class="services__item-header">
-					<?php get_template_part('theme/components/icons/service-4'); ?>
-				</div>
-				<p class="services__item-title">Programming (WordPress, Opencart)
-				</p>
-			</div>
-			<div class="services__item" data-emergence="hidden">
-				<div class="services__item-header">
-					<?php get_template_part('theme/components/icons/service-5'); ?>
-				</div>
-				<p class="services__item-title">Copywriting
-				</p>
-			</div>
-			<div class="services__item" data-emergence="hidden">
-				<div class="services__item-header">
-					<?php get_template_part('theme/components/icons/service-6'); ?>
-				</div>
-				<p class="services__item-title">Support for existing projects
-				</p>
-			</div>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </section>
