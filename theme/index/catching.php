4d4e070f49c313091d5ee371fc15734a25d13765
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-25 15:58:16 +0200

    Add catching, portfolio, services, testimonial sections

---
+++ theme/index/catching.php
@@ -0,0 +1,52 @@
+<section class="catching" id="section-catching">
+	<div class="catching__shape catching__shape_left shape shape_brand-1 shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_brand-1">
+			</div>
+		</div>
+	</div>
+	<div class="catching__shape catching__shape_right shape shape_brand-1 shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_brand-1">
+			</div>
+		</div>
+	</div>
+	<div class="catching__container container">
+		<div class="catching__row row">
+			<div class="col-12 col-sm-6">
+				<div class="group">
+					<h3 class="group__title" data-emergence="hidden" data-text="Do you like?">Do you <span class="color-brand-1">like?</span>
+					</h3>
+					<div class="group__content">
+						<p>Contact me and we will discuss all the details.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-5">
+				<div class="message message_shadow">
+					<div class="message__header">
+						<p class="message__title">Start working with me
+						</p>
+					</div>
+					<div class="message__body">
+						<form class="form form_block">
+							<div class="form__content">
+								<div class="form__row form__row_line"><input class="form__control" name="vname" type="text" placeholder="Name"/><span class="form__line"></span>
+								</div>
+								<div class="form__row form__row_line"><input class="form__control" name="vemail" type="email" placeholder="E-mail"/><span class="form__line"></span>
+								</div>
+								<div class="form__row form__row_line"><input class="form__control" name="vphone" type="phone" placeholder="Phone"/><span class="form__line"></span>
+								</div>
+								<div class="form__row">
+									<button class="btn btn_cross btn_cross-brand-1 btn_size-lg" type="submit"><span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+										<p>Send</p>
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ theme/index/testimonial.php
@@ -0,0 +1,78 @@
+<section class="testimonial" id="section-testimonial">
+	<div class="testimonial__shape testimonial__shape_right shape shape_brand-1 shape_transparent" data-emergence="hidden">
+		<div class="shape__inner">
+			<div class="shape__inner-shape shape__inner-shape_brand-1">
+			</div>
+		</div>
+	</div>
+	<div class="testimonial__container container">
+		<div class="testimonial__row row">
+			<div class="col-12 col-sm-6 col-md-7">
+				<div class="testimonial__group group">
+					<h3 class="group__title" data-emergence="hidden" data-text="Customer Testimonials">Customer <span class="color-brand-1">Testimonials</span>
+					</h3>
+					<div class="group__content">
+						<p>Customers are always happy. They know that <span class="highlight highlight_brand-1">Vsevolod Kurochka</span> is the key to success and quality.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-sm-6 col-md-5">
+				<div class="testimonial__content owl-carousel" id="owl-testimonial">
+					<div class="testimonial__item">
+						<div class="testimonial__item-header">
+							<div class="testimonial__item-image-wrapper"><img class="testimonial__item-image" src="img/image1.jpg" alt="" role="presentation"/>
+							</div>
+						</div>
+						<div class="testimonial__item-body">
+							<div class="testimonial__item-body-content">
+								<p>Man is the word! He said he did. It's nice to work, I advise everyone.</p>
+							</div>
+						</div>
+						<div class="testimonial__item-footer">
+							<p class="testimonial__item-title">Dmitry Kirpach
+							</p>
+							<p class="testimonial__item-subtitle">CEO &amp; Founder
+							</p>
+						</div>
+					</div>
+					<div class="testimonial__item">
+						<div class="testimonial__item-header">
+							<div class="testimonial__item-image-wrapper"><img class="testimonial__item-image" src="img/image2.jpg" alt="" role="presentation"/>
+							</div>
+						</div>
+						<div class="testimonial__item-body">
+							<div class="testimonial__item-body-content">
+								<p>Man is the word! He said he did. It's nice to work, I advise everyone.</p>
+							</div>
+						</div>
+						<div class="testimonial__item-footer">
+							<p class="testimonial__item-title">Dmitry Kirpach
+							</p>
+							<p class="testimonial__item-subtitle">CEO &amp; Founder
+							</p>
+						</div>
+					</div>
+					<div class="testimonial__item">
+						<div class="testimonial__item-header">
+							<div class="testimonial__item-image-wrapper"><img class="testimonial__item-image" src="img/image3.jpg" alt="" role="presentation"/>
+							</div>
+						</div>
+						<div class="testimonial__item-body">
+							<div class="testimonial__item-body-content">
+								<p>Man is the word! He said he did. It's nice to work, I advise everyone.</p>
+							</div>
+						</div>
+						<div class="testimonial__item-footer">
+							<p class="testimonial__item-title">Dmitry Kirpach
+							</p>
+							<p class="testimonial__item-subtitle">CEO &amp; Founder
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- +e.P.letters-bg.letters-bg(data-emergence="hidden") testimonials-->
+</section>
