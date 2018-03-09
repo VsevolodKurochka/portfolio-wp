b20d1b15bd4aa84581415cc445451f262e7a382f
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-03-09 13:47:52 +0200

    Fixed intro screen

--- theme/index/intro.php
+++ theme/index/intro.php
@@ -12,17 +12,20 @@
 		</div>
 	</div>
 	<div class="intro__container container">
-		<div class="intro__shape intro__shape_main shape" data-emergence="hidden">
+		<!-- <div class="intro__shape intro__shape_main shape" data-emergence="hidden">
 			<div class="shape__inner">
 				<div class="shape__inner-shape shape__inner-shape_brand-1">
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<div class="intro__row row">
 			<div class="intro__column col-12 col-sm-6 col-lg-5">
 				<h1 class="intro__title" data-emergence="hidden" data-text="Vsevolod Kurochka">Vsevolod Kurochka</h1>
-				<h2 class="intro__subtitle"><span class="intro__subtitle-element">Web developer</span><span class="intro__subtitle-element">Freelancer</span><span class="intro__subtitle-element">4 years experience</span>
-				</h2>
+				<ul class="intro__list">
+					<li>Web developer</li>
+					<li>Freelancer</li>
+					<li>4 years experience</li>
+				</ul>
 				<p class="intro__content">Looking for a web developer? I am a freelancer from Cherkassy, Ukraine. With the help of <span class="highlight highlight_brand-2">magic </span>, I will create a modern site for your business.
 				</p>
 				<div class="intro__button">
